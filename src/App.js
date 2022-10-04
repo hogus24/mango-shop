12120be7fe129e96b685e2890ed95a20febbaa90
hogus24 <jangso4@naver.com>
2022-10-04 12:00:00 +0900

    상품 등록하기 링크 연결 및 구조완성

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
+import "antd/dist/antd.css";
+import "./App.css";
+import { UploadOutlined } from "@ant-design/icons";
 import MainPage from "./components/MainPage";
 import ProductPage from "./components/ProductPage";
 import UploadPage from "./components/UploadPage";
-import "./App.css";
+import { Button } from "antd";
 
 const App = () => {
+  let navigate = useNavigate();
   return (
     <>
       <div id="header">
         <div id="header-area">
-          <img src="images/icons/logo.png" alt="logo" />
+          <Link to={"/"}>
+            <img src="images/icons/logo.png" alt="logo" />
+          </Link>
+          <Button
+            size="large"
+            icon={<UploadOutlined />}
+            onClick={() => {
+              navigate("/upload");
+            }}
+          >
+            상품 업로드
+          </Button>
         </div>
       </div>
       <Routes>
